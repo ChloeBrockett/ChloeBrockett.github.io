8073155117f4d398269bb2c090f5fd62b7e1c91e
ChloeBrockett <chloe@brockett.id.au>
2023-01-09 23:18:55 +1000

    Wanted to have something actually on the page

---
+++ src/pages/home.js
@@ -0,0 +1,22 @@
+import './styles/home.css';
+
+function Home (){
+    return(
+        <div class='main-container'>
+            <div class='hero'>
+                <h1>Chloe Brockett</h1>
+            </div>
+            <div class='socials-links'>
+                linkbox which will have links to my linkedin and github and perhaps others!!!
+            </div>
+            <div class='content'>
+                <p>Hi! I'm Chloe, a computer science student and aspiring junior dev! I'm a lifelong learner who sweats the details - I'm passionate about writing pretty code</p>
+            </div>
+            <div class='menu'>
+            </div>
+        </div>
+
+    )
+}
+
+export default Home;
