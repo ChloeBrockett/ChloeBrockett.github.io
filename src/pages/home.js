26dd44f6c464a0abeeb55e5a51d979afd9108190
Morgan Kelsie McGuire <me@morgankelsiemcguire.com>
2023-01-10 13:14:39 +1000

    Removed flex-row elements and refined flex item sizing
    Allows items to flex and collapse responsively to a single column

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,35 +8,31 @@ function Home (){
         <div class='main-container'>
             
             <div class='main-container-inner'>
-                <div class='flex-row'>
-                    <div class='header'>
-                        <h1>Chloe Brockett:</h1>
-                    </div>
-                
-                    <div class='social-links'>
-                        <div class='social-links-inner'>
-                            <a  href='https://github.com/ChloeBrockett'>
-                                <IonIcon class='social-icon' icon={logoGithub}/>
-                            </a>
-                            <a  href='mailto:chloe@brockett.id.au'>
-                                <IonIcon class='social-icon' icon={mailOutline}/>
-                            </a>
-                            <a  href='https://www.facebook.com/DominaAlgentem'>
-                                <IonIcon  class='social-icon' icon={logoFacebook}/>
-                            </a>
-                        </div>
-                    </div>
-                
+                <div class='header'>
+                    <h1>Chloe Brockett:</h1>
                 </div>
-                <div class='flex-row' id="row2">
-                    <div class='content'>
-                        <p>Hi! I'm Chloe, a computer science student and aspiring junior dev! I'm a lifelong learner who sweats the details.
-                            I love writing pretty code thats both easy to read, and simple to maintain.
-                        </p>
+            
+                <div class='social-links'>
+                    <div class='social-links-inner'>
+                        <a  href='https://github.com/ChloeBrockett'>
+                            <IonIcon class='social-icon' icon={logoGithub}/>
+                        </a>
+                        <a  href='mailto:chloe@brockett.id.au'>
+                            <IonIcon class='social-icon' icon={mailOutline}/>
+                        </a>
+                        <a  href='https://www.facebook.com/DominaAlgentem'>
+                            <IonIcon  class='social-icon' icon={logoFacebook}/>
+                        </a>
                     </div>
-                    <div class='menu'>
-                        eventually you will click stuff here and it will be sweet
-                    </div>
+                </div>
+                
+                <div class='content'>
+                    <p>Hi! I'm Chloe, a computer science student and aspiring junior dev! I'm a lifelong learner who sweats the details.
+                        I love writing pretty code thats both easy to read, and simple to maintain.
+                    </p>
+                </div>
+                <div class='menu'>
+                    eventually you will click stuff here and it will be sweet
                 </div>
             </div>
         </div>
