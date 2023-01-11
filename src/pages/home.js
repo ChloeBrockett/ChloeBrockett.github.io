c30fcacc3408e361a8fe912f5079fdf953bb7394
ChloeBrockett <chloe@brockett.id.au>
2023-01-12 02:12:33 +1000

    Added content + fixed text alignment

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState} from 'react';
+import { useState,Fragment} from 'react';
 import './styles/home.css';
 import {IonIcon} from '@ionic/react';
 import {logoFacebook,mailOutline,logoGithub} from 'ionicons/icons';
@@ -8,12 +8,32 @@ import {logoFacebook,mailOutline,logoGithub} from 'ionicons/icons';
 const projects = [
         {title:"Chloe Brockett",
             buttonText:"About Me",
-            text:"Hi! I'm Chloe, a computer science student and aspiring junior dev! I'm a lifelong learner who sweats the details. I love writing pretty code thats both easy to read, and simple to maintain."
+            text:<Fragment>
+                Hi! I'm Chloe! I'm a 3rd year computer science student and aspiring junior dev! I'm a lifelong learner who sweats the details.
+                I love writing pretty code thats both easy to read, and simple to maintain. I'm at my happiest when I've got a tough new problem 
+                to chew on, especially when the solution teaches me a new skills or a novel approach.<br /><br />
+
+
+                I've never been afraid to dive headling into challenging new experiences, both professionally and in my own time. Most recently,
+                I taught myself car repair with a 2 month deadline after my beloved miata's front end was caved in by a much larger BMW
+                </Fragment>
         },    
         {
-            title:"London",
-            buttonText:"London",
-            text:"London is pretty cool"
+            title:"Python Minimax",
+            buttonText:"Python Minimax",
+            text:    <Fragment>
+                        Technologies: Python, GitHub<br/>
+                        This was a short project to demonstrate my ability to accurately implement algorithms, competency with python, as well as how 
+                        I approach creating readable code. It is a command line app where a human player can attempt to beat the computer
+                        at noughts and crosses. <br/><br/>
+                        Because the game is solved, a perfect player will always force atleast a draw, which this app achieves via the 
+                        recursive tree search algorithm 'minimax'. The algorithm alternates between selecting the move that <strong>maximises</strong> the current
+                        players chances of victory, then the move which <strong>minimises</strong> their odds (ie: the opponents best move). It heuristically analyses
+                        game states and chooses the first best option. In future I will refactor the project to use OOP, optimise with alpha beta pruning, and
+                        add an element of randomness for more interesting gamelay<br/>
+                        <a href="https://github.com/ChloeBrockett/python-cl-naughtscrosses"><button>View Project</button></a>
+                        <a href="https://github.com/ChloeBrockett/python-cl-naughtscrosses/releases"><button>Download Executable</button></a>
+                    </Fragment>
         }
     ]
 
@@ -54,7 +74,7 @@ function Home (){
                     {projects.map((project) => (
                         <button
                         key={project.buttonText}
-                        onClick={(event)=>{
+                        onClick={()=>{
                             setActive(project.buttonText)
                             setContentText(project.text)
                             setContentHeader(project.title)
